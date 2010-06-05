f85e8d139bd3b79237c6cb7e1342fa4faeedb2d5
unknown <hirose@.(none)>
2010-06-05 16:54:50 +0900

    メニューとAbout画面追加

---
+++ src/net/assemble/emailnotify/EmailNotifyAboutActivity.java
@@ -0,0 +1,26 @@
+package net.assemble.emailnotify;
+
+import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.Bundle;
+import android.view.Window;
+
+public class EmailNotifyAboutActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_LEFT_ICON);
+
+        setContentView(R.layout.about_activity);
+
+        try {
+            PackageInfo pi = getPackageManager().getPackageInfo("net.assemble.emailnotify", 0);
+            setTitle(getResources().getString(R.string.app_name) + " ver." + pi.versionName);
+        } catch (NameNotFoundException e) {}
+        getWindow().setFeatureDrawableResource(Window.FEATURE_LEFT_ICON,
+                R.drawable.icon);
+    }
+}
--- src/net/assemble/emailnotify/EmailNotifyActivity.java
+++ src/net/assemble/emailnotify/EmailNotifyActivity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -16,16 +19,6 @@ public class EmailNotifyActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        Button btn_mail = (Button) findViewById(R.id.btn_report);
-        btn_mail.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("mailto:yh1224@gmail.com"));
-                i.putExtra(Intent.EXTRA_SUBJECT, "About EmailNotify");
-                startActivity(i);
-            }
-        });
-
         Button btn_ok = (Button) findViewById(R.id.btn_ok);
         btn_ok.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,5 +29,34 @@ public class EmailNotifyActivity extends Activity {
 
         new EmailObserver(this);
     }
+
+    /**
+     * オプションメニューの生成
+     */
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    /**
+     * オプションメニューの選択
+     */
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        Intent intent;
+        switch (itemId) {
+        case R.id.menu_report:
+            intent = new Intent(Intent.ACTION_VIEW, Uri.parse("mailto:yh1224@gmail.com"));
+            intent.putExtra(Intent.EXTRA_SUBJECT, "About EmailNotify");
+            startActivity(intent);
+            break;
+        case R.id.menu_about:
+            intent = new Intent().setClass(this, EmailNotifyAboutActivity.class);
+            startActivity(intent);
+            break;
+        }
+        return true;
+    };
 
 }
