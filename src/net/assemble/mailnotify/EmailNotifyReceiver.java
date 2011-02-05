4a943796dbe9412994dc0a362c309ab14dd0dfc6
Yoshiharu Hirose <yh1224@gmail.com>
2011-02-06 01:32:30 +0900

    アプリ更新時にサービスを再起動

--- src/net/assemble/mailnotify/EmailNotifyReceiver.java
+++ src/net/assemble/mailnotify/EmailNotifyReceiver.java
@@ -7,6 +7,7 @@ import com.android.internal.telephony.WspTypeDecoder;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.util.Log;
 
 public class EmailNotifyReceiver extends BroadcastReceiver {
@@ -47,8 +48,15 @@ public class EmailNotifyReceiver extends BroadcastReceiver {
             }
             // Restart
             else if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
-                Log.i(EmailNotify.TAG, "EmailNotify restarted.");
+                Log.i(EmailNotify.TAG, "EmailNotify restarted. (at boot)");
                 EmailNotifyService.startService(ctx);
+            } else if (intent.getAction().equals("android.intent.action.PACKAGE_REPLACED"/*Intent.ACTION_PACKAGE_REPLACED*/)) {
+                if (intent.getData() != null &&
+                    intent.getData().equals(Uri.fromParts("package", ctx.getPackageName(), null))) {
+                    // Restart service
+                    Log.i(EmailNotify.TAG, "EmailNotify restarted. (package replaced)");
+                    EmailNotifyService.startService(ctx);
+                }
             } else if (intent.getAction().equals(Intent.ACTION_TIME_CHANGED)
                     || intent.getAction().equals(Intent.ACTION_TIMEZONE_CHANGED)) {
                 EmailNotifyService.startService(ctx);
